5caaab73fe364cc06503b1442957784d9d7c9886
Nwigwe Chidubem <krain9421@gmail.com>
2024-07-02 20:07:34 +0000

    Added initial files

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Flask app module for flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Function that defines the page for / route
+    """
+    return render_template('0-index.html')
+
