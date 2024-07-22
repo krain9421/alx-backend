60d5cd65c5ea60d99d5211312b9f3c8bff273f80
Nwigwe Chidubem <krain9421@gmail.com>
2024-07-22 22:11:16 +0100

    Added files

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -36,7 +36,7 @@ class Server:
 
         return self.__dataset
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10):
         """
         Function that returns a dictionary
         containing information about a
