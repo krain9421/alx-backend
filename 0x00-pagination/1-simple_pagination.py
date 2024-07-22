6ecffc4a0bf4167c6f13629e13674817d1fe1eec
Nwigwe Chidubem <krain9421@gmail.com>
2024-07-22 12:38:55 +0100

    Added files

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Python module that implements simple pagination."""
+import csv
+import math
+from typing import List, Set, Dict, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Function takes two integer arguments and returns a tuple
+        of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list
+        for those particular pagination parameters.
+    """
+    start_index = page_size * (page - 1)
+    end_index = page_size * page
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        # Verify that both arguments are non-zero integers
+        assert page > 0 and isinstance(page, int)
+        assert page_size > 0 and isinstance(page_size, int)
+        pass
